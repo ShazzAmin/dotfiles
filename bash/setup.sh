2df01bf45672d0526566d81e69f3ddb01fb35767
Shazz <me@shazz.me>
2019-03-13 15:16:45 -0400

    Source bash dotfiles after linking

--- bash/setup.sh
+++ bash/setup.sh
@@ -8,3 +8,7 @@ echo "Symlinking bash dotfiles..."
 ln -sf $DIR/.bash_profile $HOME/.bash_profile
 ln -sf $DIR/.bashrc $HOME/.bashrc
 ln -sf $DIR/.bashrc.secret $HOME/.bashrc.secret
+
+# Load dotfiles
+echo "Loading bash dotfiles..."
+source $HOME/.bashrc
