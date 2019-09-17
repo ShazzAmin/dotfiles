1364233bcfe8aa3666fee59b13fdaa1e743ab2b8
Shazz <me@shazz.me>
2019-09-17 10:48:31 -0700

    Add Tmux Plugin Manager and Tmux Resurrect

---
+++ README.md
@@ -0,0 +1,9 @@
+dotfiles
+========
+
+*by Shazz Amin*
+
+Development environment configuration files.
+
+### Initial Set-up
+1. Run `./setup.sh`.
--- tmux/setup.sh
+++ tmux/setup.sh
@@ -6,3 +6,8 @@ DIR="$( cd "$( dirname "$0" )" > /dev/null && pwd )"
 # Symlink dotfiles
 echo "Symlinking tmux dotfiles..."
 ln -sf $DIR/.tmux.conf $HOME/.tmux.conf
+rm -f $HOME/.tmux/plugins/tpm
+mkdir -p $HOME/.tmux/plugins
+ln -sf $DIR/.tmux/plugins/tpm $HOME/.tmux/plugins/tpm
+tmux run-shell ~/.tmux/plugins/tpm/bindings/clean_plugins
+tmux run-shell ~/.tmux/plugins/tpm/bindings/install_plugins
